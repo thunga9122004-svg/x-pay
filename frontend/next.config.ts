8b8b3cb5e36ca007480ef24faea7b87b728b1317
thunga9122004-svg <thunga9122004@gmail.com>
2026-05-15 21:06:54 +0700

    ok

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
+// file: fronend/next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: '.next',
 };
 
 export default nextConfig;
